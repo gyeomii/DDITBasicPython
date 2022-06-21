808788bbcd318f50e7683ef6b7a255e6369ffbc7
gyeomii <kimsg7388@gmail.com>
2022-06-21 14:19:08 +0900

    function

--- day01/homework1.py
+++ day01/homework1.py
@@ -1,19 +1,20 @@
-from random import random
+from random import randint, random
 
 
 me = input("홀/짝을 입력하시오 : ")
-rnd = random()
+rnd = randint(0,1)
+print(rnd)
 com = ""
-if rnd >= 0.5:
+if rnd == 0:
     com ="홀"
-elif rnd < 0.5:
+elif rnd == 1:
     com = "짝"
 else :
     print("홀/짝을 정확히 입력하세요")
 
 print("user : " + me)
 print("com : " + com)
-
+print("<<결과>>")
 if me == com : 
     print("User 승리")
 else : 
