2552e1542b0227f7b9b16eeb8a2bc79eca93767c
gyeomii <kimsg7388@gmail.com>
2022-06-21 17:03:05 +0900

    oop with python

--- Hello_PYTHON/day02/myoop01.py
+++ Hello_PYTHON/day02/myoop01.py
@@ -2,15 +2,26 @@ class Animal:
     def __init__(self, age):
         self.age = age
     
-    def HappyBirth(self):
+    def happyBirth(self):
         self.age += 1
-        return self.age
+        
 
 class Human(Animal):
     def __init__(self, age, money):
         self.age = age
         self.money = money
         
-    def Albamon(self):
+    def albamon(self):
         self.money += 1
-        return self.money
+        
+# Human 객체 생성 (Animal 상속받음)
+h = Human(1, 10000)
+
+print(f"나이 : {h.age}")
+print(f"자산 : {h.money}")
+print("happyBirth, albamon 실행")
+h.albamon()
+h.happyBirth()
+
+print(f"나이 : {h.age}")
+print(f"자산 : {h.money}")
--- Hello_PYTHON/day02/myooptest.py
+++ Hello_PYTHON/day02/myooptest.py
@@ -1,5 +1,5 @@
 from day02.myoop01 import Human
 
 h = Human(1, 10000)
-print(h.HappyBirth())
-print(h.Albamon())
+print(h.happyBirth())
+print(h.albamon())
