e59e069853d249344fbf9faeb147c512a0f68fcd
gyeomii <kimsg7388@gmail.com>
2022-07-05 18:56:54 +0900

    Flask

---
+++ Hello_PYTHON/day09/myflask03.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'Hello, Flask!'
+
+
+@app.route('/forward', methods = ['POST', 'GET']) 
+def user():
+    a = 999
+    return render_template('myflask03.html', variable = a)
+
+if __name__ == '__main__':
+    app.run(debug=True)
