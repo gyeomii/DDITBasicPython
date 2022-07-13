e086e262f0dbf11e64c0ba6dcec132e3cdd8917e
gyeomii <kimsg7388@gmail.com>
2022-07-13 20:27:20 +0900

    flask

--- Hello_PYTHON/day13/myscrap03.py
+++ Hello_PYTHON/day13/myscrap03.py
@@ -8,9 +8,12 @@ response = requests.get(url)
 if response.status_code == 200:
     html = response.content.decode('euc-kr','replace')
     soup = BeautifulSoup(html, 'html.parser')
-    trArr = soup.select('tr')
-    td = trArr[0].select('td')
-    print(td[0].text)
+    attr1 = {'class' : 'st2', 'width' : '92'}
+    attr2 = {'width':'60'}
+    name = soup.find_all('td', attrs = attr1)
+    price = soup.find_all('td', attrs= attr2)
+    for idx, nm in enumerate(name):
+        print(idx, nm.text, price[idx].text)
 
 else : 
     print(response.status_code)
