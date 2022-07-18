ded8fe192c0af26e3e0260e9070bfe713077d45e
gyeomii <kimsg7388@gmail.com>
2022-07-18 10:34:18 +0900

    pyplot, numpy

---
+++ Hello_PYTHON/day16_2/my_row_col.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.arange(15).reshape(3, 5)
+
+
+a_t = np.transpose(a)
+
+
+print(a)
+
+print(a_t)
---
+++ Hello_PYTHON/day16_2/mygraph_c.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+from day16_2.daostocksink import DaoStockSink
+
+dss = DaoStockSink()
+
+
+
+
+    
+arrzs = dss.selects()    
+    
+mylen = len(arrzs[0])  
+print("mylen",mylen)  
+
+arrx = np.zeros(mylen)
+arry = list(range(mylen))
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1, projection='3d')
+
+for idx,arrz in enumerate(arrzs):
+    ax.plot(arrx+idx,arry,arrz/arrz[0])
+
+
+plt.show()
